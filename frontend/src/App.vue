e03a50028dbf67fd7e009e4311f6be0f005f0599
Onur Sahin <onursahin169@gmail.com>
2023-07-25 01:06:24 +0200

    installed headlessui

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex items-center justify-center relative">
+  <div class="flex items-center justify-center">
     <p>Hello World</p>
   </div>
 </template>
