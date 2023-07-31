0501ee5a6ad08591b7fad8159ad4d6fc952e5edd
Onur Sahin <onursahin169@gmail.com>
2023-07-31 20:16:22 +0200

    improve filechooser styles

--- frontend/src/components/Filechooser.vue
+++ frontend/src/components/Filechooser.vue
@@ -42,3 +42,12 @@ const onFileChange = (event: Event) => {
   emit("files", selectedFiles); // Emit the selectedFiles as an event
 };
 </script>
+
+<style scoped lang="postcss">
+input[type="file"]::file-selector-button {
+  @apply mr-4 px-4 py-2 border border-primary bg-white shadow cursor-pointer transition duration-200 rounded-none;
+}
+input[type="file"]::file-selector-button:hover {
+  @apply hover:bg-secondary;
+}
+</style>
