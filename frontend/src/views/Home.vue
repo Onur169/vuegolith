f7d09e9bad7f7613a7b9d2637bea23fcad66d548
Onur Sahin <onursahin169@gmail.com>
2023-08-12 20:47:45 +0200

    improve hover handling in upload list

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -16,7 +16,7 @@
         <hr class="my-6 bg-primary border border-primary" v-if="fetchedUploadsList?.length > 0" />
         <ul class="flex flex-col" v-if="fetchedUploadsList?.length > 0">
           <li
-            class="flex flex-row justify-between items-center font-regular select-none hover:bg-secondary w-12- h-12 p-3 mb-6 outline hover:outline-primary hover:shadow-md"
+            class="flex flex-row justify-between items-center font-regular select-none betterhover:hover:bg-secondary w-12- h-12 p-3 mb-6 outline hover:outline-primary hover:shadow-md"
             v-for="(uploadedFile, index) in fetchedUploadsList"
             :key="index"
           >
